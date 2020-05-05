713bc9e2c7d227c2d72172c7c133c68e450aa43f
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2020-05-05 18:31:46 -0400

    Made the first draft

---
+++ first_n_primes.py
@@ -0,0 +1,35 @@
+import doctest
+
+def is_prime(num):
+    """(num) -> bool
+    >>> is_prime(17)
+    True
+    >>> is_prime(20)
+    False
+    """
+    if(num<2):
+        return False
+    for i in range(2,int(num/2)+1):
+        if(num%i==0):
+            return False
+    return True
+
+def first_n_primes(n):
+    """(num) -> None
+    >>> first_n_primes(5)
+    2 3 5 7 11 
+    """
+    count=0
+    i=2
+    while(count<n):
+        if(is_prime(i)):
+            print(i,end=" ")
+            count+=1
+        i+=1
+
+if __name__ == "__main__":
+    doctest.testmod()
+    n=int(input("Please input an integer n: "))
+    first_n_primes(n)
+    
+    
